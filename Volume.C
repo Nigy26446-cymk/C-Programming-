ed0599d2b093daf8c41f4b95252421c87f03da91
Nigy26446-cymk <gabrielqrz62@gmail.com>
2025-09-28 01:31:57 +0300

    Update Volume.C

--- Volume.C
+++ Volume.C
@@ -1,30 +1,25 @@
-/*Name:Gabriel Kariuki
-Reg No.CT101/G/26446/25
-Description:Program to display loan application
+/*
+Name: Gabriel Kariuki
+Reg No: CT101/G/26446/25
+Decription: C Program to display the surfce area of a cylinder
 */
 
 #include <stdio.h>
 
-int main()
-{
-	int age;
-	float income;
-	
-	printf("Welcome to our loan apllication instituition\n");
-	printf("Enter your age");
-	scanf("%d", &age);
-	printf("Please enter your annual income (Sh.)");
-	scanf("%f", &income);
-	printf("Congratulations you qualify for a loan\n");
-	
-	if(age >= 21 && income >= 21000) {
-	
-	printf("You don't qyalify for a loan");
-}	
-	 
-	else
-{
-	printf("Unfortunately we are unable to offer you a loan at this time\n");	
-	}
-	return 0;
+int main () {
+ float radius;
+ float pi = 3.142;
+ float height;
+ float volume;
+ printf("Enter the radius of the cylinder/n");
+ scanf("%f", &radius);
+ 
+ printf("Enter the height of the cylinder/n");
+ scanf("%f", &height);
+ 
+ volume = pi * radius * radius * height;
+ 
+ printf("The volume of the cylinder is %f/n", volume);
+ 
+ 	return 0; 
 }
