c7b072c339519c6555386cd8f67d555bc3aec0a5
Nigy26446-cymk <gabrielqrz62@gmail.com>
2025-09-28 09:52:26 +0300

    Add files via upload

---
+++ Semester  Units.C
@@ -0,0 +1,19 @@
+/*
+Name: Gabriel Kariuki
+Reg No: CT101/G/26446/25
+Description:
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+	printf("Units offered this semester:\n");
+	printf("1. Introduction to Programming\n");
+	printf("2. Data Structures\n");
+	printf("3. Computer Networks\n");
+	printf("4. Database Systems\n");
+	printf("5. Software Engineering\n");
+	
+	return 0;
+}
